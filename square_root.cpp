beef42d63243a3e3ea5ad65d4523c793ac3cd5be
DT Pham <dt99.pham@gmail.com>
2021-08-14 20:35:34 -0700

    Add version number and configure header file

--- square_root.cpp
+++ square_root.cpp
@@ -1,12 +1,13 @@
 /* 
  * A simple program to calculate square root of a number
  */
-
+#include "TutorialConfig.h"
 #include <iostream>
 #include <cmath>
 
 int main(int argc, char **argv) {
     if (argc < 2) {
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number." << std::endl;
         return 1;
     }
